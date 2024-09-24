f24adb08250e69d142f4cf0f29610be2010744bb
higuinha <higafontourakaua@gmail.com>
2024-09-24 12:12:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var itemCardSac = document.querySelector(".card-sac");
+var btnCardSac = itemCardSac.querySelector("button");
+var contadorClick = 0;
+
+btnCardSac.addEventListener("click",function(){
+    itemCardSac.classList.toggle('ativo');
+    
+    
+})
+console.log("btnCardSac");
